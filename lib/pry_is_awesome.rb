8976403c62297e46f7c619bdcc35d764e59f513a
Michael Schneider <mic.sch91@gmail.com>
2020-06-05 20:18:07 +0000

    Automatically backed up by Learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -7,7 +7,7 @@ def prying_into_the_method
 	this_variable_hasnt_been_interpreted_yet = "The program froze before it could read me!"
 	puts this_variable_hasnt_been_interpreted_yet
 	num = 3
-	puts num * 2
+	num1 = num * 2
 	binding.pry
 end
 
